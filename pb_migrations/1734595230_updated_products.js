f1bae3ad7070036654843bf6871722cc949fd702
Gonzalo Sibolich <gonzaesc2303@gmail.com>
2024-12-19 05:29:37 -0300

    faztcode pocketbase nextjs 15:34

---
+++ pb_migrations/1734595230_updated_products.js
@@ -0,0 +1,89 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_4092854851")
+
+  // update collection data
+  unmarshal({
+    "name": "productos"
+  }, collection)
+
+  // add field
+  collection.fields.addAt(1, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text982552870",
+    "max": 0,
+    "min": 0,
+    "name": "nombre",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(2, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text2687119104",
+    "max": 0,
+    "min": 0,
+    "name": "descripcion",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(3, new Field({
+    "hidden": false,
+    "id": "number380223906",
+    "max": null,
+    "min": null,
+    "name": "precio",
+    "onlyInt": false,
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "number"
+  }))
+
+  // add field
+  collection.fields.addAt(4, new Field({
+    "hidden": false,
+    "id": "bool2882213148",
+    "name": "activo",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "bool"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_4092854851")
+
+  // update collection data
+  unmarshal({
+    "name": "products"
+  }, collection)
+
+  // remove field
+  collection.fields.removeById("text982552870")
+
+  // remove field
+  collection.fields.removeById("text2687119104")
+
+  // remove field
+  collection.fields.removeById("number380223906")
+
+  // remove field
+  collection.fields.removeById("bool2882213148")
+
+  return app.save(collection)
+})
